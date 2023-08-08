4353bad885b66b2247f05e8b7e7cfc133cc952b6
Ennio <ennioh@icloud.com>
2023-08-08 00:17:26 -0400

    Scroll infinito do feed finalizado

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,17 +6,22 @@ import Error from '../../Helper/Error';
 import Loading from '../../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ pages: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
+      const total = 6;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { response, json } = await request(url, options);
+      console.log('json');
       console.log(json);
+      if (response && response.ok && json.length < total) {
+        setInfinite(false);
+      }
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
@@ -24,7 +29,11 @@ const FeedPhotos = ({ setModalPhoto }) => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosIem key={photo.id} photo={photo} setModalPhoto={setModalPhoto}/>
+          <FeedPhotosIem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
